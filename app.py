4ac57a432774179c4dfcca12a1bb6525f9199596
Dewansh Yadav <dewanshyadaw8@gmail.com>
2024-01-21 14:49:08 +0530

    Improvised

--- app.py
+++ app.py
@@ -4,29 +4,36 @@ import os
 import PyPDF2 as pdf
 from dotenv import load_dotenv
 
-
-load_dotenv() ## load all our environment variables
+load_dotenv()
+# load all our environment variables
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 
-
 def get_gemini_repsonse(input):
-    model=genai.GenerativeModel('gemini-pro')
-    response=model.generate_content(input)
-    return response.text
+    model = genai.GenerativeModel('gemini-pro')
+    res = model.generate_content(input)
+    return res.text
+
 
 def input_pdf_text(uploaded_file):
-    reader=pdf.PdfReader(uploaded_file)
-    text=""
+    reader = pdf.PdfReader(uploaded_file)
+    txt = ""
     for page in range(len(reader.pages)):
-        page=reader.pages[page]
-        text+=str(page.extract_text())
-    return text
+        page = reader.pages[page]
+        txt += str(page.extract_text())
+    return txt
 
-#Prompt Template
 
-input_prompt="""
+# streamlit app
+st.title("Smart ATS by Dewansh")
+# st.text("Improve Your Resume ATS")
+text = ""
+jd = st.text_area("Paste the Job Description")
+
+# Prompt Template
+
+input_prompt = """
 Hey Act Like a skilled or very experience ATS(Application Tracking System)
 with a deep understanding of tech field,software engineering,data science, machine learning, Devops ,data analyst
 and big data engineer. Your task is to accurately evaluate the resume based on the given job description.
@@ -44,16 +51,12 @@ MissingKeywords:[]
 Improvements:""
 """
 
-## streamlit app
-st.title("Smart ATS by Dewansh")
-# st.text("Improve Your Resume ATS")
-jd=st.text_area("Paste the Job Description")
-uploaded_file=st.file_uploader("Upload Your Resume",type="pdf",help="Please uplaod the pdf")
+uploaded_file = st.file_uploader("Upload Your Resume", type="pdf", help="Please upload the pdf")
 
 submit = st.button("Submit")
 
 if submit:
     if uploaded_file is not None:
-        text=input_pdf_text(uploaded_file)
-        response=get_gemini_repsonse(input_prompt)
+        text = input_pdf_text(uploaded_file)
+        response = get_gemini_repsonse(input_prompt)
         st.subheader(response)
